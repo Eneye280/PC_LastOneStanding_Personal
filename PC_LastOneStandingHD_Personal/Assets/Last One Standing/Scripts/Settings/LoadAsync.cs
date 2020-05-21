6d92d5809cfddb6160e0c68c301bbd50c4fd1b4d
Euclides Martinez <eneye280@gmail.com>
2020-05-20 23:37:21 -0500

    Add UI
    * Add UI For Panel Game Play
    * Add UI and Settings For Scene Load Asyn

---
+++ PC_LastOneStandingHD_Personal/Assets/Last One Standing/Scripts/Settings/LoadAsync.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class LoadAsync : MonoBehaviour
+{
+    private AsyncOperation asyncOperation;
+
+    [SerializeField] internal bool isLoadStart;
+    [SerializeField] internal bool isPanelBackgroundImage;
+
+    [Header("Parameter's Progress")]
+    [SerializeField] internal Slider sliderLoadAsyn;
+    [Range(0, 100)] 
+    [SerializeField] internal float howMuchIsTheProgress;
+    [SerializeField] internal string nameTextProgress;
+    [SerializeField] internal TextMeshProUGUI textInProgress;
+    [SerializeField] internal TextMeshProUGUI textPhrasesRandom;
+    [TextArea(0,50)]
+    [SerializeField] internal string[] phrasesRandom;
+    
+    [Header("Panel Backgroun")]
+    [SerializeField] internal GameObject panelLoadAsync;
+    [SerializeField] internal Sprite[] backgroundLoadAsyn;
+
+
+
+    private void Awake()
+    {
+        if (isPanelBackgroundImage)
+        {
+            panelLoadAsync.SetActive(false);
+            panelLoadAsync.GetComponent<Image>().sprite = backgroundLoadAsyn[Random.Range(0, backgroundLoadAsyn.Length)]; 
+        }
+
+        textPhrasesRandom.text = phrasesRandom[Random.Range(0, phrasesRandom.Length)];
+
+        if (isLoadStart)
+            StartCoroutine(LoadScene("SceneLobby"));
+    }
+
+    public void LoadButton(string scene)
+    {
+        StartCoroutine(LoadScene(scene));
+    }
+
+    private IEnumerator LoadScene(string scene)
+    {
+        yield return null;
+
+        asyncOperation = SceneManager.LoadSceneAsync(scene);
+
+        asyncOperation.allowSceneActivation = false;
+
+        while (!asyncOperation.isDone)
+        {
+            if(isPanelBackgroundImage)
+                panelLoadAsync.SetActive(true);
+  
+            textInProgress.text = nameTextProgress + (asyncOperation.progress * howMuchIsTheProgress) + "%";
+            sliderLoadAsyn.value = asyncOperation.progress;
+            asyncOperation.allowSceneActivation = true;
+
+            yield return null;
+        }
+    }
+}
