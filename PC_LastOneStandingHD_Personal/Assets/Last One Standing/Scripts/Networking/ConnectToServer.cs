c4107314cafcdb32bbd276e9584698524f2ac1e7
Euclides Martinez <eneye280@gmail.com>
2020-06-17 22:59:41 -0500

    Add Panel Start Game Play

---
+++ PC_LastOneStandingHD_Personal/Assets/Last One Standing/Scripts/Networking/ConnectToServer.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        print("conecting to server...");
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("conected to server");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("disconected from server for season " + cause.ToString());
+    }
+}
